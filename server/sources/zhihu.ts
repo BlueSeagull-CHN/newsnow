93d50e25617fb5f55be6349574ddad2b0527b6c7
BlueSeagull <blueseagull@860411.xyz>
2025-11-02 17:21:17 +0800

    Fix URL in Zhihu source for hot stories

--- server/sources/zhihu.ts
+++ server/sources/zhihu.ts
@@ -36,7 +36,7 @@ interface Res {
 
 export default defineSource({
   zhihu: async () => {
-    const url = "https://www.zhihu.com/api/v3/feed/topstory/hot-list-web?limit=20&desktop=true"
+    const url = "https://www.zhihu.com/api/v3/feed/top_story/hot_list_web?limit=20&desktop=true"
     const res: Res = await myFetch(url)
     return res.data
       .map((k) => {
